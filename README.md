712317ed9b60cf9fa01910b25cf707650f79bef0
utgwkk <utagawakiki@gmail.com>
2020-11-12 15:50:47 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Perl::Critic::Policy::ControlStructures::ProhibitReturnInDoBlock - It's new $module
+
+# SYNOPSIS
+
+    use Perl::Critic::Policy::ControlStructures::ProhibitReturnInDoBlock;
+
+# DESCRIPTION
+
+Perl::Critic::Policy::ControlStructures::ProhibitReturnInDoBlock is ...
+
+# LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+utgwkk <utagawakiki@gmail.com>
---
+++ lib/Perl/Critic/Policy/ControlStructures/ProhibitReturnInDoBlock.pm
@@ -0,0 +1,39 @@
+package Perl::Critic::Policy::ControlStructures::ProhibitReturnInDoBlock;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Perl::Critic::Policy::ControlStructures::ProhibitReturnInDoBlock - It's new $module
+
+=head1 SYNOPSIS
+
+    use Perl::Critic::Policy::ControlStructures::ProhibitReturnInDoBlock;
+
+=head1 DESCRIPTION
+
+Perl::Critic::Policy::ControlStructures::ProhibitReturnInDoBlock is ...
+
+=head1 LICENSE
+
+Copyright (C) utgwkk.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+utgwkk E<lt>utagawakiki@gmail.comE<gt>
+
+=cut
+
